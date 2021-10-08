6ef9cbbc8cf672eadcfebc9f498df4b7ba991646
Tom Hohl <tom.hohl@gmail.com>
2021-10-08 16:15:45 -0600

    bugfix

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ApplicationRecord
   has_many :tags, through: :question_tags
 
   def self.find_question_by_tag(tag_name)
-    select('q.id AS question_id, t.id AS tag_id, q.title AS title, t.name AS Tag, q.body AS body')
+    select('q.id, t.id AS tag_id, q.title, t.name, q.body, q.created_at')
     .from('questions AS q')
     .joins('INNER JOIN question_tags AS qt ON q.id=qt.question_id
     INNER JOIN tags AS t ON qt.tag_id=t.id')

740e931e5f644c6c5dad8ea3dd3d019291d7131b
Michaela Sivertsen <mjsivertsen@msn.com>
2021-10-27 00:35:45 -0600

    I'm sorry, I get anal about instructions

--- client/src/components/Markdown/MarkdownEditor.js
+++ client/src/components/Markdown/MarkdownEditor.js
@@ -16,13 +16,13 @@ const MarkdownEditor = ({body,setBody}) => {
         1. When adding code to a question or answer, you need to wrap your code in backticks.  If copying and pasting code, you can highlight the code (once pasted) and click the {'</>'} button to automatically insert backticks around your code.
         </Modal.Body>
         <Modal.Body>
-        2.  If there is only one line of code, a set of single backticks will suffice.  However, it is recommended that you use triple backticks if your code has any backticks in its content, even if it is only one line.  Otherwise, the backticks in your code content will need to be wrapped in a set of curly brackets and quotes.  In other words, you would need to type {"{'`'}"} instead of just {'`'}.
+        2.  If there is only one line of code, a set of single backticks will suffice.  However, it is recommended that you use triple backticks, especially if your code has any backticks in its content.  Otherwise, the backticks in your code content will need to be wrapped in a set of curly brackets and quotes.  In other words, you would need to type {"{'`'}"} instead of just {'`'}.
         </Modal.Body>
         <Modal.Body>
         3. Clicking the {'</>'} button before adding lines of code will insert a set of single backticks. If highlighting code and then clicking the {'</>'} button, the number of backticks inserted will be appropriate for the length of code.  If there is only one line of code, single backticks will be inserted.  If there is more than one line of code, a set of triple backticks will be inserted.
         </Modal.Body>
         <Modal.Body>
-        4. If using triple backticks to show your code, you can add a language after the initial set of backticks to display the code differently. Otherwise, the styling for displaying code will be JavaScript.
+        4. When using triple backticks to show your code, you can add a language after the initial set of backticks to display the code differently (for example, {'```'} ruby). Otherwise, the styling on your codeblock will be optimized for JavaScript.
         </Modal.Body>
         <Modal.Footer>
           <DPLButton onClick={()=>setShowHelp(!showHelp)}>
